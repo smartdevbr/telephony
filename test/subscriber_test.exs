f355683ad3b18256dabccdf43ba59b5de9f87a8b
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-03-12 06:47:39 -0300

    all the tests is 100%

--- test/subscriber_test.exs
+++ test/subscriber_test.exs
@@ -14,12 +14,16 @@ defmodule SubscriberTest do
     end
 
     @subscribers("subscribers.txt")
-    @subscriber_update %Subscriber{name: "Update ok", number: "11234", plan: "pos"}
+    @subscriber_update %Subscriber{name: "Update ok", number: "11234", plan: %PostPaid{value: nil}}
 
 
     describe "tests for creating a subscriber" do 
-        test "create a subscriber" do 
+        test "create a prepaid subscriber" do 
             assert Subscriber.create("Gustavo", "1197778490", :pre) == :ok
+        end
+
+        test "create a postpaid subscriber" do 
+            assert Subscriber.create("Gustavo", "1197778490", :post) == :ok
         end
         
         test "throw a message when subscriber registered" do 
@@ -28,32 +32,44 @@ defmodule SubscriberTest do
         end
     end
 
-    test "delete a subscriber" do 
-        assert Subscriber.delete("1197778490") == :ok
+    describe "find_by_number" do
+        test "should return a subscriber " do 
+            Subscriber.create("Gustavo", "1197778490", :pre)
+            assert Subscriber.find_by_number("1197778490").name == "Gustavo"
+        end
+
+        test "should return a prepaid subscriber " do 
+            Subscriber.create("Gustavo", "1197778490", :pre)
+            assert Subscriber.find_by_number("1197778490", :pre).name == "Gustavo"
+        end
+
+        test "should return a postpaid subscriber " do 
+            Subscriber.create("Gustavo", "1197778490", :post)
+            assert Subscriber.find_by_number("1197778490", :post).name == "Gustavo"
+        end
+
     end
 
-    test "should return a subscriber " do 
-        Subscriber.create("Gustavo", "1197778490", "pre")
-        assert Subscriber.find_by_number("1197778490").name == "Gustavo"
+    describe "update" do
+        test "update subscriber" do
+            Subscriber.create("Test update", "11234", :post)
+            assert Subscriber.update("11234", @subscriber_update) == :ok
+        end
+
+        test "should throw a message error" do
+            Subscriber.create("Test update", "11234", :pre)
+            assert Subscriber.update("11234", @subscriber_update) == "Subscriber needs to have the same plan"
+        end
     end
 
-    test "should read a file_name" do 
-        [first_element | _tail] = Subscriber.read_file(@subscribers)
-        assert first_element.name == "Josh"
+    describe "delete" do
+        test "delete a subscriber" do 
+            Subscriber.create("Gustavo", "1197778490", :pre)
+            assert Subscriber.delete("1197778490") == :ok
+        end
     end
 
-    test "should throw a error message when try to read a file does not exist" do
-        assert Subscriber.read_file("subscribers") == "Failed to read the file"
-    end
 
-    test "should return a subscriber in find_by_number" do 
-        Subscriber.create("Gustavo", "1197778490", "pre")
-        assert Subscriber.find_by_number("1197778490").name == "Gustavo"
-    end
 
-    test "update subscriber" do
-        Subscriber.create("Test update", "11234", "pos")
-        assert Subscriber.update("11234", @subscriber_update) == :ok
-    end
     
 end
