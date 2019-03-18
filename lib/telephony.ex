b3a27b9c7e2b14c0c66707b8878ae375d5d554b4
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-03-18 08:11:59 -0300

    make a new recharge

--- lib/telephony.ex
+++ lib/telephony.ex
@@ -3,4 +3,9 @@ defmodule Telephony do
   def hello(name) do
     "Hello #{name}"  
   end
+
+  def start() do
+    File.write("pre.txt", :erlang.term_to_binary([]))
+    File.write("post.txt", :erlang.term_to_binary([]))
+  end
 end
