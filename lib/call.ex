c5db9df343caa116471ad2757f8130d1445526f3
Gustavo Oliveira <oliveira.gustavo@totvs.com.br>
2019-03-18 08:48:35 -0300

    register a subscriber call

---
+++ lib/call.ex
@@ -0,0 +1,14 @@
+defmodule Call do
+    defstruct date: nil, duration: nil
+
+    def register_call(subscriber, cost, date, duration) do
+       plan = subscriber.plan
+       plan = %Prepaid{plan | credits: plan.credits - cost}
+
+       subscriber_update = %Subscriber{subscriber | 
+        plan: plan,
+        calls: subscriber.calls ++ [%Call{date: date, duration: duration}]
+        }
+        Subscriber.update(subscriber.number, subscriber_update)
+    end
+end
--- lib/prepaid.ex
+++ lib/prepaid.ex
@@ -1,13 +1,17 @@
 defmodule Prepaid do
+    
+    @price_per_minute 1.45
+
     defstruct credits: 0, recharges: []
+
 
     def make_call(number, date, duration) do 
         subscriber = Subscriber.find_by_number(number, :pre)
-        cost = 1.45 * duration
+        cost = @price_per_minute * duration
 
         cond do
             cost <= subscriber.plan.credits -> 
-                "you can make the call"
+                Call.register_call(subscriber, cost, date, duration)
             true ->
                 "You are not allowed to make the call. please make a recharge!"
         end
